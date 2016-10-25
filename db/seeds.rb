1cc2748ba7f94a7f1b986281f67487f17bb32201
Will Farrell <email": null>
2016-10-25 07:56:20 -0500

    Seed states branch to make it playable

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,8 @@
 CSV.foreach("#{APP_ROOT}/country_coords.csv", col_sep: "\t", :headers => true, :header_converters => :symbol) do |row|
-    p row[:short_name]
     Country.create(name: row[:short_name], latitude: row[:lat].to_i, longitude: row[:long].to_i)
+end
+
+
+CSV.foreach("#{APP_ROOT}/state_latlon.csv", :headers => true, :header_converters => :symbol) do |row|
+    State.create(name: row[:name], abbreviation: row[:state], latitude: row[:latitude].to_i, longitude: row[:longitude].to_i)
 end
